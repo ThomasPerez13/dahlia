dcbbcd93466144735b80167b62de98f2b4e3fe7a
Marwan Belhora <marwan.belhora@audencia.com>
2022-09-01 14:53:55 +0200

    fonts et login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :tel_number, presence: true
-  validates :email, presence: true
-  validates :gender, presence: true
-
-  validates :email, format: { with: /\A.*@.*\.com\z/ }
+  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
 end
