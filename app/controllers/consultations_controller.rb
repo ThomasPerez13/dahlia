27725d78fe7f8d582558498b8f831f5056dd57c7
raphael hamou <hamou.raphael@gmail.com>
2022-09-05 11:32:01 +0200

    new consultation in progress..

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -7,4 +7,22 @@ class ConsultationsController < ApplicationController
     @consultation = Consultation.find(params[:id])
     @notes = @consultation.notes
   end
+
+  def new
+    @consultation = Consultation.new
+  end
+
+  def create
+    @consultation = Consultation.new(consultation_params)
+    @consultation.user = current_user
+    @consultation.save
+
+    redirect_to consultation_path(@consultation)
+  end
+
+  private
+
+  def consultation_params
+    params.require(:consultation).permit(:start_date, :patient_id)
+  end
 end
