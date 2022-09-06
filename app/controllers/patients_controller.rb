9042481658c4b000cb7f597a318ea238badcdf75
raphael hamou <hamou.raphael@gmail.com>
2022-09-06 11:32:51 +0200

    fff

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,13 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def index
+    @patients = Patient.where(referring_user: current_user)
+    if params[:query].present?
+      @patients = @patients.where("first_name ILIKE ?", "%#{params[:query]}%")
+    else
+      @patients
+    end
+  end
 end
