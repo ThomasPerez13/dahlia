aef358f0c3af4ce28f19d1a492288c3169c1c636
raphael hamou <hamou.raphael@gmail.com>
2022-09-08 15:13:19 +0200

    Je viens de finir le design ombragé des consultations faites

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form"]
 
-  connect() {
-    console.log('hello')
-  }
-
   submit(event) {
     event.preventDefault()
     this.element.submit()
