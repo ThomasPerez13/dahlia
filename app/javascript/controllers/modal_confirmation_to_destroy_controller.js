fe586d6bf9a2bc5ed96151da41d6d661d23eeb93
benoit brandt <benoit.07@hotmail.com>
2023-02-03 15:11:58 +0100

    delete console log in stimulus

--- app/javascript/controllers/modal_confirmation_to_destroy_controller.js
+++ app/javascript/controllers/modal_confirmation_to_destroy_controller.js
@@ -5,12 +5,11 @@ export default class extends Controller {
   static targets = ["modal"]
 
   connect() {
-    console.log("ready to destroy something ?");
+
   }
 
   displayModal(event) {
     event.preventDefault()
     this.modalTarget.classList.toggle("d-none")
-    console.log("Do you really want to destroy it ?");
   }
 }
