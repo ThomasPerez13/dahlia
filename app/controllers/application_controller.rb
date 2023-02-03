2c3f4b07c0bff48a5fc1b4b0de2fbf25ec624cbc
benoit brandt <benoit.07@hotmail.com>
2023-02-03 15:11:26 +0100

    add comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # add info flash configure in the partial shared/flashes
   add_flash_types :info
 
   def configure_permitted_parameters
