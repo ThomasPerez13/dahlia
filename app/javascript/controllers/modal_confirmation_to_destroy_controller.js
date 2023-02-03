31aa7838177fbe91db4f8be5b73dfd4deaa11ed3
benoit brandt <benoit.07@hotmail.com>
2023-02-03 12:31:06 +0100

    create destroy confirmation modal

--- app/javascript/controllers/modal_confirmation_to_destroy_controller.js
+++ app/javascript/controllers/modal_confirmation_to_destroy_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal-confirmation-to-destroy"
 export default class extends Controller {
+  static targets = ["modal"]
+
   connect() {
     console.log("ready to destroy something ?");
   }
 
   displayModal(event) {
     event.preventDefault()
+    this.modalTarget.classList.toggle("d-none")
     console.log("Do you really want to destroy it ?");
   }
 }
