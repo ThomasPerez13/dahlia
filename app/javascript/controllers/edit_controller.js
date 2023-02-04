ab3658279a7a002c762586d7fe91e3b11f9292ec
benoit brandt <benoit.07@hotmail.com>
2023-02-04 17:13:27 +0100

    fix bug to edit nurse of a consultation and style it

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["form", "button", "formNurse"]
-  // connect() {
-  //   console.log("Hello")
-  // }
+  connect() {
+    console.log("Hello")
+  }
 
   displayForm() {
     this.formTarget.classList.toggle("d-none");
   }
+
   displayFormNurse() {
     this.formNurseTarget.classList.toggle("d-none");
   }
