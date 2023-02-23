65554956d3989d5c1e69941079bb2e271fde7c98
benoit brandt <benoit.07@hotmail.com>
2023-02-23 17:09:29 +0100

    display another modal to delete group consultation or treatment

--- app/javascript/controllers/modal_confirmation_to_destroy_controller.js
+++ app/javascript/controllers/modal_confirmation_to_destroy_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal-confirmation-to-destroy"
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["modal", "modalDestroyGroup"]
 
   connect() {
 
@@ -11,5 +11,14 @@ export default class extends Controller {
   displayModal(event) {
     event.preventDefault()
     this.modalTarget.classList.toggle("d-none")
+  }
+
+  displayModalToDestroyGroup(event) {
+    event.preventDefault()
+    console.log(this.modalTarget.classList.contains("d-none"));
+    if (!this.modalTarget.classList.contains("d-none")) {
+      this.modalTarget.classList.toggle("d-none")
+    }
+    this.modalDestroyGroupTarget.classList.toggle("d-none")
   }
 }
